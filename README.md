0048c516dc8903d1d042aa9a7fcea1a227990e2e
liuquanchang <liuquanchang@qianxin.com>
2021-11-24 19:22:26 +0800

    update redeme.md

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # ThreadPool
+
+## 主要参考
+[progschj/ThreadPool](https://github.com/progschj/ThreadPool)
+
+[lzpong/threadpool](https://github.com/lzpong/threadpool)
+
+主要对线程池访问加锁，多线程提交任务可能会有并发问题
+
+## 支持仿函数
+```
+struct TestMin
+{
+	template <typename Ty>
+	constexpr const int operator()(const Ty& n1, const Ty& n2) const noexcept
+	{
+		return n1 < n2;
+	}
+};
+
+vecRes.push_back(std::move(ThreadPool::instance().CommitTask(
+	std::bind(TestMin(), (rand() % 100), (rand() % 101)))
+));
+
+vecRes.push_back(std::move(ThreadPool::instance().CommitTask(
+	TestMin(), "test1", "test2"
+	)
+));
+```
+## 支持不定参数
+```
+static const auto function = [](int a, int b, int thread_index)
+{
+	std::cout << "This is thread :[" << thread_index << "]" << ",thread id:[" << std::this_thread::get_id() << "]" << std::endl;
+	std::this_thread::sleep_for(SECOND(10));
+	return a * b;
+};
+
+vecRes.push_back(std::move(ThreadPool::instance().CommitTask(
+			std::bind(function, (rand() % 100), (rand() % 101), 0))
+	));
+```
--- ThreadPoolTest/main.cpp
+++ ThreadPoolTest/main.cpp
@@ -4,6 +4,8 @@
 #include <thread>
 #include <chrono>
 #include "../ThreadPool.h"
+#define SECOND(n) std::chrono::seconds(n)
+#define MILL_SECOND(n) std::chrono::milliseconds(n)
 
 class TimeConsum
 {
@@ -18,42 +20,81 @@ private:
 	int64_t m_nTimeStart{ 0 };
 };
 
+struct TestMin
+{
+	template <typename Ty>
+	constexpr const int operator()(const Ty& n1, const Ty& n2) const noexcept
+	{
+		return n1 < n2;
+	}
+};
+
 void Test()
 {
-	using std::chrono::operator"" s;
+	static const auto function = [](int a, int b, int thread_index)
+	{
+		std::cout << "This is thread :[" << thread_index << "]" << ",thread id:[" << std::this_thread::get_id() << "]" << std::endl;
+		std::this_thread::sleep_for(SECOND(10));
+		return a * b;
+	};
 
 	std::vector<std::future<int>> vecRes;
 	TimeConsum timec;
 
-	auto function = [](int a, int b, int thread_index)
+	for (int i = 0; i < 10; ++i)
 	{
-		std::cout << "This is thread :[" << thread_index << "]" << std::endl;
-		std::this_thread::sleep_for(10s);
-		return a * b;
-	};
-
-	for (int i = 0; i<100; ++i)
-	{
-		std::this_thread::sleep_for(0.1s);
+		std::this_thread::sleep_for(MILL_SECOND(100));
 		vecRes.push_back(std::move(ThreadPool::instance().CommitTask(
 			std::bind(function, (rand() % 100), (rand() % 101), i))
 		));
 	}
 
-	std::this_thread::sleep_for(10s);
 	for (size_t i = 0; i < vecRes.size(); ++i)
 	{
 		std::cout << "Res of :[" << i << "]" << std::endl;
 		std::cout << vecRes[i].get() << std::endl;//阻塞调用
 	}
 
-	std::cout << "Const time:[" << timec << "]" << std::endl;
+	std::cout << "Const time:[" << timec << "]" << ",thread id:[" << std::this_thread::get_id() << "]" << std::endl;
+}
+
+void Test_1()
+{
+	std::vector<std::future<int>> vecRes;
+	TimeConsum timec;
+
+	for (int i = 0; i < 10; ++i)
+	{
+		std::this_thread::sleep_for(MILL_SECOND(100));
+		vecRes.push_back(std::move(ThreadPool::instance().CommitTask(
+			std::bind(TestMin(), 1, 2))
+		));
+	}
+
+	for (int i = 0; i < 10; ++i)
+	{
+		std::this_thread::sleep_for(MILL_SECOND(100));
+		vecRes.push_back(std::move(ThreadPool::instance().CommitTask(
+			TestMin(), "test1", "test2"
+			)
+		));
+	}
+
+	for (size_t i = 0; i < vecRes.size(); ++i)
+	{
+		std::cout << "Res of :[" << i << "]" << std::endl;
+		std::cout << vecRes[i].get() << std::endl;//阻塞调用
+	}
+
+	std::cout << "Const time:[" << timec << "]" << ",thread id:[" << std::this_thread::get_id() << "]" << std::endl;
 }
 
 int main()
 {
-	Test();
-
+	std::thread t1(Test);
+	std::thread t2(Test_1);
+	t1.join();
+	t2.join();
 	return 0;
 }
 
